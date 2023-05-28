e10dd75a3c70e14031d4045f29a74afb3983ea07
AliElgohary <alielgohary35@gmail.com>
2023-05-28 19:08:35 +0300

    script file

---
+++ script.js
@@ -0,0 +1,6 @@
+function add() {
+    let num1 = Number(document.getElementById("num1").value);
+    let num2 = Number(document.getElementById("num2").value);
+    let sum = Number(num1 + num2);
+    document.getElementById("result").innerHTML = sum;
+  }
